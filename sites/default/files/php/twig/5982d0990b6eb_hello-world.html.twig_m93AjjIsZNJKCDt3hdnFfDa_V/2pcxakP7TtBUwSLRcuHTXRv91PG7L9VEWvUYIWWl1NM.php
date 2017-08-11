a4888bc7dd2738f2b92de45170fdd4c6864950b7
Alex Mozh <alfa.bel@mail.ru>
2017-08-11 16:51:16 +0300

    settings

---
+++ sites/default/files/php/twig/5982d0990b6eb_hello-world.html.twig_m93AjjIsZNJKCDt3hdnFfDa_V/2pcxakP7TtBUwSLRcuHTXRv91PG7L9VEWvUYIWWl1NM.php
@@ -0,0 +1,72 @@
+<?php
+
+/* modules/custom/hello_world/templates/hello-world.html.twig */
+class __TwigTemplate_922a40f951ed00826b671bb077f46b53345d09ce3dc70a3939d1ddd4b1ee4ebf extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<p>Test twig template!</p>
+
+<p>test_var: ";
+        // line 3
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["test_var"]) ? $context["test_var"] : null), "html", null, true));
+        echo "</p>";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/custom/hello_world/templates/hello-world.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  47 => 3,  43 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "<p>Test twig template!</p>
+
+<p>test_var: {{ test_var }}</p>";
+    }
+}
